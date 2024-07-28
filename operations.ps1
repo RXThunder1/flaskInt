b3eeca9832b80cce6cf39259b110d914721b4d7f
RXThunder1 <162224845+RXThunder1@users.noreply.github.com>
2024-07-27 21:15:39 -0700

    Add files via upload

---
+++ operations.ps1
@@ -0,0 +1,16 @@
+# calc/operations.ps1
+function add($a, $b) {
+    return $a + $b
+}
+
+function sub($a, $b) {
+    return $a - $b
+}
+
+function mult($a, $b) {
+    return $a * $b
+}
+
+function div($a, $b) {
+    return $a / $b
+}
